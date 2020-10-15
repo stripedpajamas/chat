1b907b4181f4e1991df7ac4a31435a094a75ea05
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-10-15 16:37:18 -0700

    Update server.js to use playground data

--- server.js
+++ server.js
@@ -1,49 +1,46 @@
 const Fastify = require('fastify')
 const got = require('got')
-const MsgStore = require('./msgstore')
-const config = require('./config')(process.argv[2])
+const Node = require('./playground/node.js')
+const config = require('./config.js')(process.argv[2]) // node server.js <id>
 
 const fastify = Fastify({ logger: true })
-fastify.decorate('state', new MsgStore())
+fastify.decorate('node', new Node({ logger: fastify.log }))
 fastify.decorate('nodes', new Set())
 
-// get the current value
-fastify.get('/msglist', function (req, reply) {
-  const msgList = this.state.allMsgs()
-  reply.send({ msglist: [...msgList.values()] })
+fastify.get('/messages', function (req, reply) {
+  reply.send({ messages: this.node.getMessages() })
 })
 
-// add a msg
-fastify.post('/add', function (req, reply) {
-  const { msg } = req.body
-  const timestamp = Date.now()
-  this.state.add({ timestamp, ...msg })
-  reply.send({ ok: 1 })
+fastify.post('/messages', {
+  preValidation: function (req, reply, done) {
+    // TODO ensure only self can hit this route
+    done()
+  },
+  handler: function (req, reply) {
+    const { text, channel } = req.body
+    this.node.addMsg({ text, channel })
+    reply.send({ ok: 1 })
+  }
 })
 
-// sync state with another node
 fastify.post('/sync', function (req, reply) {
-  const { deltas } = req.body
-  this.state.mergeSerializedDeltas(deltas)
+  const { syncData } = req.body
+  this.node.db.mergeSyncData(syncData)
   reply.send({ ok: 1 })
 })
 
-// register another node so this node can sync with it
 fastify.post('/register', function (req, reply) {
-  const { address } = req.body
+  const { id, address } = req.body
+  this.node.addLogId(id)
   this.nodes.add(address)
   reply.send({ ok: 1})
 })
 
-// every 5s, sync with known nodes
 setInterval(() => {
-  if (fastify.state.getDeltas().size) {
-    const deltas = fastify.state.getSerializedDeltas()
-    for (const node of fastify.nodes) {
-      got.post(`${node}/sync`, { json: { deltas } })
-        .then(() => fastify.state.clearDeltas())
-        .catch((e) => fastify.log.error(e))
-    }
+  const syncData = fastify.node.db.getSyncData()
+  for (const node of fastify.nodes) {
+    got.post(`${node}/sync`, { json: { syncData } })
+      .catch((e) => fastify.log.warn(e)) // not a big deal if sync fails (sibling node down)
   }
 }, 5000)
 
